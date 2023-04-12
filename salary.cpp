66aaa627b92b5f919514c804af395491d385a16b
Sadra <mo.sadra.abbasi@gmail.com>
2023-04-12 18:06:00 +0430

    private variable

--- salary.cpp
+++ salary.cpp
@@ -6,6 +6,22 @@
 #include <map>
 #include <algorithm>
 
+
+enum commands
+{
+    REPORT_SALARIES,
+    REPORT_EMPLOYEE_SALARY,
+    REPORT_TEAM_SALARY,
+    REPORT_TOTAL_HOURS_PER_DAY,
+    REPORT_EMPLOYEE_PER_HOUR,
+    SHOW_SALARY_CONFIG,
+    UPDATE_SALARY_CONFIG,
+    ADD_WORKING_HOURS,
+    DELETE_WORKING_HOURS,
+    UPDATE_TEAM_BONUS,
+};
+
+
 using namespace std;
 
 class Day
@@ -15,16 +31,10 @@ public:
     {
         day = init_day;
     }
-    void set_interval_working(int start_time, int end_time) //*********
-    {
-        string start = to_string(start_time);
-        string end = to_string(end_time);
-        interval_working = start + '-' + end;
-    }
 
 private:
     int day;
-    string interval_working;
+    pair<int , int> interval_working;
 };
 
 class Salary_Configs
@@ -60,6 +70,35 @@ private:
     float bonus_working_hours_max_variance;
 };
 
+
+
+
+
+int read_command_convert_to_int(string input)
+{
+    if (input == "report_salies")
+        return REPORT_SALARIES;
+    if (input == "report_employee_salary")
+        return REPORT_EMPLOYEE_SALARY;
+    if (input == "report_team_salary")
+        return REPORT_TEAM_SALARY;
+    if (input == "report_total_hours_per_day")
+        return REPORT_TOTAL_HOURS_PER_DAY;
+    if (input == "report_employee_per_hour")
+        return REPORT_EMPLOYEE_PER_HOUR;
+    if (input == "show_salary_config")
+        SHOW_SALARY_CONFIG;
+    if (input == "update_salary_config")
+        UPDATE_SALARY_CONFIG;
+    if (input == "add_working_hours")
+        return ADD_WORKING_HOURS;
+    if (input == "delete_working_hours")
+        return DELETE_WORKING_HOURS;
+    if (input == "update_team_bonus")
+        UPDATE_TEAM_BONUS;
+}
+
 int main()
 {
+
 }
